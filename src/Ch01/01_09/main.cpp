f40a72d138de142a0282905cc68553ee8005daee
Ahmed Yousry <arc.ahmedyousry@gmail.com>
2025-08-12 11:43:51 +0000

    Lesson 9: Console input / output

--- src/Ch01/01_09/main.cpp
+++ src/Ch01/01_09/main.cpp
@@ -4,13 +4,14 @@
 
 #include <iostream>
 #include <string>
+#include "sharedFns.h"
 
 int main(){
     std::string name;
-    std::cout << "Enter your name: " << std::flush;
+    std::cout << "Please enter your name: " << std::flush;
     std::cin >> name;
-    std::cout << "Nice to meet you, " << name << "!" << std::endl;
+    std::cout << "Welcome " << name << std::endl;
 
-    std::cout << std::endl << std::endl;
+    printFooter();
     return 0;
 }
