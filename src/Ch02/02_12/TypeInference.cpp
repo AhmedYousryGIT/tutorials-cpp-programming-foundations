9a02b70188f9cd6fc66a859bc2bebbe05c8c4cab
Ahmed Yousry <arc.ahmedyousry@gmail.com>
2025-08-16 12:15:37 +0300

    Chapter 2 Lesson 12 About auto detection of data types

--- src/Ch02/02_12/TypeInference.cpp
+++ src/Ch02/02_12/TypeInference.cpp
@@ -4,16 +4,21 @@
 
 #include <iostream>
 #include <typeinfo>
+#include "sharedFns.h"
 
 int main(){
-    auto score;
-    auto points;
-    auto height;
-    auto duration;
-    auto is_active;
-    auto initial;
-    auto title;
-
+    printSpacer();
+    //auto must be initialized with a value.
+    //Uncommenting the line below gives an error
+    //auto score;
+    auto score = 8;
+    auto points = 12345678901;
+    auto height = 6.2f;
+    auto duration = 90.0;
+    auto is_active = true;
+    auto initial = 'P';
+    auto title = "Soccer Champions";
+    
     std::cout << "The type of score is " << typeid(score).name() << std::endl;
     std::cout << "The type of points is " << typeid(points).name() << std::endl;
     std::cout << "The type of height is " << typeid(height).name() << std::endl;
@@ -21,7 +26,7 @@ int main(){
     std::cout << "The type of is_active is " << typeid(is_active).name() << std::endl;
     std::cout << "The type of initial is " << typeid(initial).name() << std::endl;
     std::cout << "The type of title is " << typeid(title).name() << std::endl;
-
-    std::cout << std::endl << std::endl;
+    
+    printSpacer();
     return 0;
 }
