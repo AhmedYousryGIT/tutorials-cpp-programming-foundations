9cd38400055efd49878c56b7d911793eeead3ea1
Ahmed Yousry <arc.ahmedyousry@gmail.com>
2025-08-18 16:10:15 +0300

    Chapter 3 Lesson 6 Complete Selective code execution Using #define and #ifdef blocks to selectively enable / disable blocks of code

--- src/Ch03/03_06/SelectiveCodeExecution.cpp
+++ src/Ch03/03_06/SelectiveCodeExecution.cpp
@@ -4,17 +4,30 @@
 
 #include <iostream>
 #include <cstdint>
+#include "sharedFns.h"
+
+//comment the line below to stop debugging functions
+#define DEBUG
+
+const uint16_t MAX_AMMO = 500;
 
 int main(){
+    printSpacer();
+    
     int32_t ammo = 100;
     uint8_t health_items = 5;
 
+//Debugging block of code. Only runs if DEBUG is defined
+#ifdef DEBUG:
+    std::cout << "[DEBUG] Debugging mode" << std::endl;
+#endif
+
     ammo += 200; // Player finds extra ammo
     health_items -= 2; // Player uses some health items
-
+    
     std::cout << "Final Ammo: " << ammo << std::endl;
     std::cout << "Remaining Health Items: " << (int) health_items << std::endl;
-
-    std::cout << std::endl << std::endl;
+    
+    printSpacer();
     return 0;
 }
