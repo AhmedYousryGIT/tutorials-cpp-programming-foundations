02c1a8e78c4492c749ecde28d39ddd168b028498
Ahmed Yousry <arc.ahmedyousry@gmail.com>
2025-08-30 16:54:13 +0300

    Chapter 4 Lesson 10 C++ string class. Safer, more powerful and efficient than the C style strings.

--- src/Ch04/04_10/CppString.cpp
+++ src/Ch04/04_10/CppString.cpp
@@ -4,19 +4,22 @@
 
 #include <iostream>
 #include <string>
+#include "sharedFns.h"
 
 int main(){
+    printSpacer();
+    
+    const size_t LENGTH = 50;
     std::string racer1 = "Speedy";
     std::string racer2 = "Lightning";
     std::string raceResult;
-
+    
     std::cout << "Racer 1: " << racer1 << std::endl;
     std::cout << "Racer 2: " << racer2 << std::endl;
-
+    
     raceResult = "And the winner is " + racer1;
-
     std::cout << raceResult << std::endl;
-
-    std::cout << std::endl << std::endl;
+    
+    printSpacer();
     return 0;
 }
